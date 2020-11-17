2eca9546cc5343ee953046fccca09f25af562932
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-11-17 17:53:47 +0900

    Rubocopの実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.last_name}
-    email {Faker::Internet.free_email}
-    password { "ss0000" }
-    password_confirmation { "ss0000" }
-    last_name { "山田" }
-    first_name { "太郎" }
-    last_name_kana { "ヤマダ"}
-    first_name_kana { "タロウ" }
-    birth_date { "1950-11-11" }
+    nickname { Faker::Name.last_name }
+    email { Faker::Internet.free_email }
+    password { 'ss0000' }
+    password_confirmation { 'ss0000' }
+    last_name { '山田' }
+    first_name { '太郎' }
+    last_name_kana { 'ヤマダ' }
+    first_name_kana { 'タロウ' }
+    birth_date { '1950-11-11' }
   end
 end
