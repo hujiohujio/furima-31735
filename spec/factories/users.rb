1e59d3d8dd52992d8502d6af135227b64842233a
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-11-17 17:50:19 +0900

    userモデルのテスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { "ss0000" }
+    password_confirmation { "ss0000" }
     last_name { "山田" }
     first_name { "太郎" }
     last_name_kana { "ヤマダ"}
