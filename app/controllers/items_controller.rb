aef20b99f560466a32e491488edf4fc46bd478b1
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-11-17 23:47:56 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,4 +7,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :product_discription, :category_id, :status_id, :shipping_fee_id, :shipping_prefecture_id, :scheduled_delivery, :price, :image).merge(user_id: current_user.id)  
+  end
 end
