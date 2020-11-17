1fa10f2c8db30ac61bfa5d45ce85fffd0f42ee8e
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-11-18 01:34:30 +0900

    商品出品ページへの遷移実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,16 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :order
   has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :product_description
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :shipping_prefecture_id
+    validates :scheduled_delivery_id
+    validates :image
+    validates :price, inclusion: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
