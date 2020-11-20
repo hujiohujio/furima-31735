b7dfd321685591e1c5c2fd53a3d3455a5317afab
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-11-20 13:04:06 +0900

    商品詳細ページの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -16,6 +16,11 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :product_description, :category_id, :status_id, :shipping_fee_id, :shipping_prefecture_id, :scheduled_delivery_id, :price, :image).merge(user_id: current_user.id)
