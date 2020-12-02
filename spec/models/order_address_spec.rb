1b8db626a6ec04c0bc52faa5157e2367d8d957e1
hujiohujio <hujio_hujio0100@yahoo.co.jp>
2020-12-02 11:36:20 +0900

    rubocopの実施

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -15,9 +15,9 @@ RSpec.describe OrderAddress, type: :model do
       expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
     end
     it 'postal_codeにハイフンが無い場合保存できない' do
-      @order_address.postal_code = "1111111"
+      @order_address.postal_code = '1111111'
       @order_address.valid?
-      expect(@order_address.errors.full_messages).to include("Postal code is invalid")
+      expect(@order_address.errors.full_messages).to include('Postal code is invalid')
     end
     it 'prefecture_idが空の場合保存できない' do
       @order_address.prefecture_id = nil
